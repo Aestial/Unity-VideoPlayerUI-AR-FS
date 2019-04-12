d6a1f403cf57fd08e245b9a39fda2cd0f511d990
Jaime Hernandez <jaimevazz@gmail.com>
2019-04-12 15:54:23 -0500

    Added Skins

---
+++ Assets/VideoPlayerAR/Scripts/Core/Skins/FullscreenHUDSkin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName =  "New Fullscreen HUD Skin", menuName = "Fullscreen Skin")]
+public class FullscreenHUDSkin : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public Color backgroundColor;
+    public Color buttonsColor;
+    public Color panelsColor;
+    public Color scrubBallColor;
+
+    //public Sprite playButton;
+    //public Sprite pauseButton;
+    //public Sprite loopButton;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/VideoPlayerAR/Scripts/Fulllscreen/FullscreenHUDController.cs
+++ Assets/VideoPlayerAR/Scripts/Fulllscreen/FullscreenHUDController.cs
@@ -1,14 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RA.Video
 {
     public class FullscreenHUDController : MonoBehaviour
     {
+        [Header("Canvas elements for show and hide")]
         [SerializeField] Canvas videoCanvas;
         [SerializeField] Canvas controlCanvas;
-        [SerializeField] HUDAppearBehaviour m_HUDAppear;
+        //[SerializeField] HUDAppearBehaviour m_HUDAppear;
+
+        [Header("Skin object and customizable UI elements")]
+        [SerializeField] FullscreenHUDSkin skin;
+        [SerializeField] Image backgroundImage;
+        [SerializeField] Image scrubBallImage;
+        [SerializeField] Image[] panelsImages;
+        [SerializeField] Image[] buttonsImages;
+
+        void Start()
+        {
+            Debug.Log("SKIN Configuration");
+            Debug.Log("Skin name: " + skin.name);
+            Debug.Log("Skin description: " + skin.description);
+
+            backgroundImage.color = skin.backgroundColor;
+            scrubBallImage.color = skin.scrubBallColor;
+            SetImageArrayColors(panelsImages, skin.panelsColor);
+            SetImageArrayColors(buttonsImages, skin.buttonsColor);
+
+        }
+
+        private void SetImageArrayColors(Image[] images, Color color)
+        {
+            for (int i = 0; i < images.Length; i++)
+            {
+                images[i].color = color;
+            }
+        }
 
         public void Show(bool enabled)
         {
